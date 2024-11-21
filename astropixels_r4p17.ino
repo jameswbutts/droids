c3505e30f29839bfabf510730da556b770e47e14
jameswbutts <jameswbutts@gmail.com>
2024-11-21 15:46:38 -0600

    Update astropixels_r4p17.ino

--- astropixels_r4p17.ino
+++ astropixels_r4p17.ino
@@ -16,10 +16,39 @@
 
 I2CReceiver i2cReceiver(0x0a);
 
-AstroPixelRLD<> RLD(LogicEngineRLDDefault, 3);
-AstroPixelFLD<> FLD(LogicEngineFLDDefault, 1);
+//AstroPixelRLD<> RLD(LogicEngineRLDDefault, 3);
+//AstroPixelFLD<> FLD(LogicEngineFLDDefault, 1);
+static LogicEngineSettings LogicEngineRLDCustom(
+    LogicEngineDefaults::FRONT_FADE,
+    LogicEngineDefaults::FRONT_HUE,
+    LogicEngineDefaults::FRONT_DELAY,
+    3,//LogicEngineDefaults::FRONT_PAL,
+    LogicEngineDefaults::FRONT_BRI,
+    LogicEngineDefaults::sequence(LogicEngineDefaults::NORMAL));
+AstroPixelRLD<> RLD(LogicEngineRLDCustom, 1);
 
-AstroPixelFrontPSI<> frontPSI(LogicEngineFrontPSIDefault, 4);
+static LogicEngineSettings LogicEngineFLDCustom(
+    LogicEngineDefaults::FRONT_FADE,
+    LogicEngineDefaults::FRONT_HUE,
+    LogicEngineDefaults::FRONT_DELAY,
+    3,//LogicEngineDefaults::FRONT_PAL,
+    LogicEngineDefaults::FRONT_BRI + 30,
+    LogicEngineDefaults::sequence(LogicEngineDefaults::NORMAL));
+AstroPixelFLD<> FLD(LogicEngineFLDCustom, 1);
+
+
+LogicEngineSettings LogicEngineFrontPSICustom(
+    LogicEngineDefaults::FRONT_FADE,
+    LogicEngineDefaults::FRONT_HUE,
+    LogicEngineDefaults::FRONT_DELAY,
+    LogicEngineDefaults::FRONT_PSI_PAL,
+    LogicEngineDefaults::FRONT_BRI + 60,
+    LogicEngineDefaults::sequence(LogicEngineDefaults::PSICOLORWIPE,LogicEngineDefaults::kCyan)
+    );
+
+AstroPixelFrontPSI<> frontPSI(LogicEngineFrontPSICustom, 4);
+
+//AstroPixelFrontPSI<> frontPSI(LogicEngineFrontPSIDefault, 4);
 AstroPixelRearPSI<> rearPSI(LogicEngineRearPSIDefault, 5);
 
 HoloLights frontHolo(25, HoloLights::kRGB);
@@ -33,7 +62,7 @@ void setup()
     REELTWO_READY();
     SetupEvent::ready();
     RLD.selectScrollTextLeft("... Hello There ....", LogicEngineRenderer::kBlue, 0, 15);
-    FLD.selectScrollTextLeft("... R2-D2 ...", LogicEngineRenderer::kBlue, 0, 15);
+    FLD.selectScrollTextLeft("... R4-P17 ...", LogicEngineRenderer::kRed, 0, 15);
     CommandEvent::process("HPA0026|20");
 }
 
